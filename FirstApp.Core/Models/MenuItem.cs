e13aadeff62532c84f021277d7bf05704b9e5071
Евгений Храмов <Evgenkhramov@gmail.com>
2019-03-17 00:04:19 +0200

    recycler view

---
+++ FirstApp.Core/Models/MenuItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstApp.Core.Models
+{
+    public class Item
+    {
+        public string ItemName { get; set; }
+    }
